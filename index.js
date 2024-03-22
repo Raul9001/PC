41df75852818ec442ecebf39a4c1f4066d114c43
raul9001 <raul.ansarov@mail.ru>
2024-03-23 03:28:41 +0400

    pc

---
+++ index.js
@@ -0,0 +1,45 @@
+const overlayMenu=document.querySelector(".overlay-menu")
+const navbarButton=document.querySelector(".navbar-toggler-icon")
+const closemenu=document.querySelector(".fa-x")
+
+
+navbarButton.addEventListener("click",openthemenu)
+closemenu.addEventListener("click", closethemenu)  
+
+
+
+
+function openthemenu(){
+  overlayMenu.classList.add("aktiv")
+  
+}
+
+function closethemenu(){
+ overlayMenu.classList.remove("aktiv")
+
+}
+
+
+// swiper js code
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  }
+});
